d17596315b5f0ca590170668de1c355e6eb642eb
Joa Roque <joaquim@pinaculodigital.com.br>
2024-12-08 02:09:01 +0100

    feat: implement client service

---
+++ backend/src/services/client.service.js
@@ -0,0 +1,42 @@
+import { PrismaClient } from "@prisma/client";
+
+const prismaClient = new PrismaClient();
+
+export const createClient = async (username, password, cpf) => {
+  const client = await prismaClient.$transaction(async (prisma) => {
+    const user = await prisma.user.create({
+      data: {
+        username,
+        password,
+      },
+    });
+
+    const client = await prisma.client.create({
+      data: {
+        userId: user.id,
+        cpf,
+      },
+    });
+  });
+};
+
+export const listTransactions = async (clientId) => {
+  return prismaClient.transaction.findMany({
+    where: { clientId },
+  });
+};
+
+export const addPix = async (clientId, amount) => {
+  return prismaClient.transaction.create({
+    data: {
+      clientId,
+      amount,
+    },
+  });
+};
+
+export const getUserByUsername = async (username) => {
+  return prismaClient.user.findFirst({
+    where: { username },
+  });
+};
