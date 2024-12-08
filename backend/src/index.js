d5776a48ec29d335041766bfc7e3f9ffb43361a1
Joa Roque <joaquim@pinaculodigital.com.br>
2024-12-08 03:36:35 +0100

    setup app

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,6 +1,7 @@
 import cors from "cors";
 import "dotenv/config.js";
 import express from "express";
+import clientRouter from "./routes/client.router.js";
 
 const PORT = process.env.PORT || 3000;
 const app = express();
@@ -8,6 +9,9 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
+app.use("/api", clientRouter);
+
+// @ts-ignore
 app.get("/", (req, res) => {
   return res.json({ message: "Teste técnico ValidaPix" }).sendStatus(200);
 });
